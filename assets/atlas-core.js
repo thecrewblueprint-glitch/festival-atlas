fdf8abd5fa0d28e4db00eb75e18fcce8ae7b6bd1
thecrewblueprint-glitch <thecrewblueprint@gmail.com>
2026-06-21 21:48:26 -0400

    Delete unused legacy core file

--- assets/atlas-core.js
+++
@@ -1,8 +0,0 @@
-(function(){
-  if(window.__atlasCoreCompatibilityLoaderInstalled)return;
-  window.__atlasCoreCompatibilityLoaderInstalled=true;
-  var script=document.createElement('script');
-  script.src='assets/atlas-core-v2.js?v=multi2';
-  script.async=false;
-  document.head.appendChild(script);
-})();
